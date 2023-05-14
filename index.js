284750ac61ec1f7abb95279db498f4a85ae459ae
Rohit Ingole <ingolerohit2017@gmail.com>
2023-05-14 13:07:11 +0530

    chnges

---
+++ index.js
@@ -0,0 +1,15 @@
+//PROBLEM: CHECK EVEN ODD USING FUNCTION
+
+function checkEvenOdd(number){
+    if(number%2==0)
+    {
+        console.log("Even");
+    }
+    else
+    {
+        console.log("odd");
+    }
+}
+checkEvenOdd(5);
+checkEvenOdd(4);
+checkEvenOdd(10); 
